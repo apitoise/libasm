1118848de73aa5e068904d3e84792fa1e952a73c
apitoise <arthur.pitoiset@outlook.fr>
2024-08-06 11:38:21 +0200

    better main

--- main.c
+++ main.c
@@ -16,19 +16,25 @@ int			main(int ac, char **argv) {
 	char	dst[MAX_LEN];
 	char	*ft_ret, *real_ret;
 	char	*bad_str = "imabadstring";
+	char	*empty_str = "";
 
 	if (ac != 2)
 		return (exit_error("Need at least 1 arg"));
+	printf("#### STRLEN ####\n\n");
 	printf("ft_strlen: %lu\n", ft_strlen(argv[1]));
 	printf("strlen: %lu\n", strlen(argv[1]));
+	printf("\n\n#### STRCPY ####\n\n");
 	ft_ret = ft_strcpy(dst, argv[1]);
-	printf("ft_strcpy: '%s' | '%s'\n", dst, ft_ret);
+	printf("ft_strcpy: %s | %s\n", dst, ft_ret);
 	real_ret = strcpy(dst, argv[1]);
-	printf("strcpy: '%s' | '%s'\n", dst, real_ret);
+	printf("strcpy: %s | %s\n", dst, real_ret);
+	printf("\n\n#### STRCMP ####\n\n");
 	printf("good ft_strcmp: %i\n", ft_strcmp(ft_ret, real_ret));
 	printf("good strcmp: %i\n", strcmp(ft_ret, real_ret));
 	printf("bad ft_strcmp: %i\n", ft_strcmp(ft_ret, bad_str));
 	printf("bad strcmp: %i\n", strcmp(ft_ret, bad_str));
+	printf("empty ft_strcmp: %i\n", ft_strcmp(ft_ret, empty_str));
+	printf("empty strcmp: %i\n", strcmp(ft_ret, empty_str));
 
 	return (0);
 }
