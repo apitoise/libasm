f0ea959b73e5e7c37349c0cb7ea40d1e0f975e69
apitoise <arthur.pitoiset@outlook.fr>
2024-08-07 11:28:22 +0200

    laggy save

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 extern size_t	ft_strlen(const char *str);
 extern char		*ft_strcpy(char *dst, const char *src);
@@ -17,30 +19,57 @@ static int	exit_error(const char *str) {
 
 int			main(int ac, char **argv) {
 	char	dst[MAX_LEN];
-	char	buff[MAX_LEN];
 	char	*ft_ret, *real_ret;
 	char	*bad_str = "imabadstring";
 	char	*empty_str = "";
 
 	if (ac != 2)
 		return (exit_error("Need at least 1 arg"));
+
 	printf("#### STRLEN ####\n\n");
-	printf("ft_strlen: %lu\n", ft_strlen(argv[1]));
-	printf("strlen: %lu\n", strlen(argv[1]));
+
+	printf("ft_strlen:\n%lu\n", ft_strlen(argv[1]));
+	printf("strlen:\n%lu\n", strlen(argv[1]));
+
 	printf("\n\n#### STRCPY ####\n\n");
+
 	ft_ret = ft_strcpy(dst, argv[1]);
-	printf("ft_strcpy: %s | %s\n", dst, ft_ret);
+	printf("ft_strcpy:\n%s | %s\n", dst, ft_ret);
 	real_ret = strcpy(dst, argv[1]);
-	printf("strcpy: %s | %s\n", dst, real_ret);
+	printf("strcpy:\n%s | %s\n", dst, real_ret);
+
 	printf("\n\n#### STRCMP ####\n\n");
-	printf("good ft_strcmp: %i\n", ft_strcmp(ft_ret, real_ret));
-	printf("good strcmp: %i\n", strcmp(ft_ret, real_ret));
-	printf("bad ft_strcmp: %i\n", ft_strcmp(ft_ret, bad_str));
-	printf("bad strcmp: %i\n", strcmp(ft_ret, bad_str));
-	printf("empty ft_strcmp: %i\n", ft_strcmp(ft_ret, empty_str));
-	printf("empty strcmp: %i\n", strcmp(ft_ret, empty_str));
+
+	printf("good ft_strcmp:\n%i\n", ft_strcmp(ft_ret, real_ret));
+	printf("good strcmp:\n%i\n", strcmp(ft_ret, real_ret));
+	printf("bad ft_strcmp:\n%i\n", ft_strcmp(ft_ret, bad_str));
+	printf("bad strcmp:\n%i\n", strcmp(ft_ret, bad_str));
+	printf("empty ft_strcmp:\n%i\n", ft_strcmp(ft_ret, empty_str));
+	printf("empty strcmp:\n%i\n", strcmp(ft_ret, empty_str));
+
 	printf("\n\n#### WRITE ####\n\n");
-	ft_write(1, buff, ft_strlen(buff));
-	write(1, buff, strlen(buff));
+
+	int ft_fd = open("ft_write_fd", O_APPEND | O_CREAT | O_WRONLY, 0644);
+	int fd = open("write_fd", O_APPEND | O_CREAT | O_WRONLY, 0644);
+
+	printf("Errno_value: %d\nErrno_str: %s\n", errno, strerror(errno));
+	printf("ft_write stdout:\n");
+	ft_write(STDOUT_FILENO, argv[1], ft_strlen(argv[1]));
+	printf("\n");
+	printf("write stdout:\n");
+	write(STDOUT_FILENO, argv[1], strlen(argv[1]));
+	printf("\n");
+	printf("ft_write good fd:\n");
+	ft_write(ft_fd, argv[1], ft_strlen(argv[1]));
+	printf("\n");
+	printf("write good fd:\n");
+	write(fd, argv[1], strlen(argv[1]));
+	printf("\n");
+	printf("ft_write wrong fd:\n");
+	ft_write(4242, argv[1], ft_strlen(argv[1]));
+	printf("\n");
+	printf("write wrong fd:\n");
+	write(4242, argv[1], strlen(argv[1]));
+	printf("\n");
 	return (0);
 }
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -2,15 +2,15 @@ global	ft_write
 extern	__errno_location
 
 ft_write:
-	mov		rax,	1			; set write syscall
-	syscall						; syscall write
-	test	rax,	rax			;
-	js		.error				; if ret < 0 jump to .error func
-	ret							; else return ret
+	mov		rax,	1						; set write syscall
+	syscall									; syscall write
+	test	rax,	rax						;
+	js		.error							; if ret < 0 jump to .error func
+	ret										; else return ret
 
 .error:
-	call	__errno_location	; call errno_location
-	mov		rbx,	rax			; put errno_location addr in rbx
-	mov		[rbx],	rax			; put err_code in rbx to set errno var glob
-	mov		rax,	-1			;
-	ret							; return -1
+	mov		r8,		rax
+	call	__errno_location wrt ..plt		; call errno
+	mov		[rax],	r8						; set errno value
+	mov		rax,	-1						;
+	ret										; return -1
